c5d4619ee4fa40363ec0966b2bafe4377d71d8d9
Borna Cvitanić <borna.cvitanic@gmail.com>
2024-07-21 11:51:07 +0200

    Update main.rs to add capability of opening lazygits of packages and the packages folder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::fmt;
 use structopt::StructOpt;
@@ -42,12 +42,41 @@ fn main() {
 
     if args.project_dir.is_dir() {
         match args.project_type {
-            ProjectType::Unity => { open_sln_file(&args.project_dir); }
+            ProjectType::Unity => { open_unity_project(&args.project_dir); }
             ProjectType::Rust => { open_rust_project(&args.project_dir); }
         }
     } else {
         eprintln!("Provided path is not a directory.")
     }
+}
+
+fn open_unity_project(project_path: &Path){
+    utils::open_unity_project(&project_path);
+    open_sln_file(&project_path);
+    utils::open_lazygit(&project_path);
+    let packages_path = project_path.join("Packages");
+    utils::open_directory(&packages_path);
+    let packages = get_packages(&packages_path);
+    for package in &packages {
+        utils::open_lazygit(&package);
+    }
+}
+
+fn get_packages(packages_path: &Path) -> Vec<PathBuf>{
+    let mut packages = Vec::new();
+    for entry  in fs::read_dir(&packages_path).unwrap() {
+        match entry {
+            Ok(entry) => {
+                let package_path = entry.path();
+                let package_json = package_path.join("package.json");
+                if package_path.is_dir() && package_json.exists() {
+                    packages.push(package_path);
+                }
+            }
+            Err(e) => eprintln!("Error reading package entry: {}, Error: {}", packages_path.display(), e)
+        }
+    }
+    packages
 }
 
 fn open_sln_file(project_path: &Path){
@@ -57,8 +86,6 @@ fn open_sln_file(project_path: &Path){
         if file_path.extension().unwrap_or_default() == "sln" {
             println!("Opening solution file: {}", file_path.display());
             utils::open_file(&file_path);
-            utils::open_lazygit(&project_path);
-            utils::open_unity_project(&project_path);
             return;
         }
     }
